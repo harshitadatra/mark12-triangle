9f0d293230e62316d279defa9a7e4a53ec91ae2a
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2021-08-23 17:45:05 +0530

    creates: hypotenuse.js

---
+++ hypotenuse.js
@@ -0,0 +1,20 @@
+var sideLength = document.querySelectorAll(".side-length");
+var checkButton = document.querySelector("#check-button");
+var message = document.querySelector("#display-message");
+
+checkButton.addEventListener("click",function clickHandler()
+{
+    // console.log("clicked");
+    var sum = sumOfSquares(Number(sideLength[0].value),Number(sideLength[1].value));
+    var lengthOfHypotenuse = Math.sqrt(sum);
+    showMessage(`the length of the hypotenuse is ${lengthOfHypotenuse}`);
+})
+function sumOfSquares(a,b)
+{
+    var sum = a*a + b*b
+    return sum;
+}
+function showMessage(msg)
+{
+    message.innerText= msg;
+}

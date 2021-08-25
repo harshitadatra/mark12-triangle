d083bf6ce9a4ddbb13e0e81503f27510d786c291
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2021-08-25 15:00:45 +0530

    created: quiz.js

---
+++ quiz.js
@@ -0,0 +1,33 @@
+const formInput =document.querySelector(".quiz-form");
+const message =document.querySelector("#display-message");
+const checkButton = document.querySelector("#check-button")
+const correctAnswers =["45","right angle"];
+ checkButton.addEventListener("click", function clickHandler()
+ {
+    calculateScore();
+
+
+ });
+ function calculateScore()
+ {
+     var formResults = new FormData(formInput);
+     var score=0;
+     var index=0;
+     for(let values of formResults.values())
+     {
+         if(values == correctAnswers[index])
+         {
+             score= score+1;
+             index=index+1;
+         }
+     }
+    showMessage (`the score is ${score}`);
+
+ }
+  function showMessage(msg)
+  {
+      message.innerText = msg;
+  }
+ 
+
+ 
